ff2cad221460e1f239306bb1698bb48347c8df1c
Ismael-Salazar <is2520@columbia.edu>
2020-10-02 20:55:14 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,16 +1,15 @@
 public class Card implements Comparable<Card> {
 	
-	//Instance variables
 	private int suit;
 	private int value;
 
-	//Makes a card
+	//Creates a "card."
 	public Card(int s, int v) {
 		suit = s;
 		value = v;
 	}
 
-	//Compares cards
+	//Compares card values.
 	public int compareTo(Card c) {
 		int measurement = 0;
 		if (this.value < c.value)
@@ -28,18 +27,17 @@ public class Card implements Comparable<Card> {
 		return measurement;
 	}
 
-	//returns value
+	//Returns card value.
 	public int getValue(){
 		return value;
 	}
 	
-	//Returns suit
+	//Returns card suit.
 	public int getSuit(){
 		return suit;
 	}
 	
-	//Converts to string
-	//I made an ace 14 for everything
+	//Return string representation of a card.
 	public String toString() {
 		String[] suits = {"Clubs","Diamonds","Hearts","Spades"};
 		String[] values = {"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
